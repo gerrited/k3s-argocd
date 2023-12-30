ee86e8f91f5c35ed72ef0b59960059a60a0f1513
Gerrit Edzards <gerrit.edzards@gmail.com>
2023-12-30 13:02:40 +0100

    Change tf organization

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,11 +2,11 @@
 terraform {
     backend "remote" {
     # The name of your Terraform Cloud organization.
-    organization = "example-organization"
+    organization = "k3s-g11s"
 
     # The name of the Terraform Cloud workspace to store Terraform state files in.
     workspaces {
-        name = "example-workspace"
+        name = "k3s"
     }
     }
 }
